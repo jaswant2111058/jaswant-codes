ee48f605e9feac668ef80a401822867e95c105eb
jaswant2111058 <jaswant2111058@akgec.ac.in>
2022-10-06 13:57:08 +0530

    Bingo

---
+++ jk.js
@@ -0,0 +1,36 @@
+function fac()
+{
+    const arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25];
+ 
+    {
+      arr.sort (function(){return 0.5 -Math.random()});
+    }
+  document.getElementById("a0").value = arr[0];
+  document.getElementById("a1").value = arr[1];
+  document.getElementById("a2").value = arr[2];
+  document.getElementById("a3").value = arr[3];
+  document.getElementById("a4").value = arr[4];
+  document.getElementById("b0").value = arr[5];
+  document.getElementById("b1").value = arr[6];
+  document.getElementById("b2").value = arr[7];
+  document.getElementById("b3").value = arr[8];
+  document.getElementById("b4").value = arr[9];
+  document.getElementById("c0").value = arr[10];
+  document.getElementById("c1").value = arr[11];
+  document.getElementById("c2").value = arr[12];
+  document.getElementById("c3").value = arr[13];
+  document.getElementById("c4").value = arr[14];
+  document.getElementById("d0").value = arr[15];
+  document.getElementById("d1").value = arr[16];
+  document.getElementById("d2").value = arr[17];
+  document.getElementById("d3").value = arr[18];
+  document.getElementById("d4").value = arr[19];
+  document.getElementById("e0").value = arr[20];
+  document.getElementById("e1").value = arr[21];
+  document.getElementById("e2").value = arr[22];
+  document.getElementById("e3").value = arr[23];
+  document.getElementById("e4").value = arr[24];
+  
+  
+  
+}
