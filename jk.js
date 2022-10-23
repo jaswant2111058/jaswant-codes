577a967804a30aeb86e5329f1ab7383721e7a88c
jaswant2111058 <jaswant2111058@akgec.ac.in>
2022-10-23 23:04:28 +0530

    NEW VERSION

--- jk.js
+++ jk.js
@@ -1,36 +1,240 @@
+var arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25];
+  document.getElementById("a0").innerHTML = "B";
+  document.getElementById("a1").innerHTML = "I";
+  document.getElementById("a2").innerHTML = "N";
+  document.getElementById("a3").innerHTML = "G";
+  document.getElementById("a4").innerHTML = "O";
+  document.getElementById("b0").innerHTML = "B";
+  document.getElementById("b1").innerHTML = "I";
+  document.getElementById("b2").innerHTML = "N";
+  document.getElementById("b3").innerHTML = "G";
+  document.getElementById("b4").innerHTML = "O";
+  document.getElementById("c0").innerHTML = "B";
+  document.getElementById("c1").innerHTML = "I";
+  document.getElementById("c2").innerHTML = "N";
+  document.getElementById("c3").innerHTML = "G";
+  document.getElementById("c4").innerHTML = "O";
+  document.getElementById("d0").innerHTML = "B";
+  document.getElementById("d1").innerHTML = "I";
+  document.getElementById("d2").innerHTML = "N";
+  document.getElementById("d3").innerHTML = "G";
+  document.getElementById("d4").innerHTML= "O";
+  document.getElementById("e0").innerHTML= "B";
+  document.getElementById("e1").innerHTML = "I";
+  document.getElementById("e2").innerHTML = "N";
+  document.getElementById("e3").innerHTML = "G";
+  document.getElementById("e4").innerHTML = "O";
 function fac()
+  {
+   arr.sort(function(){return 0.5- Math.random()});
+  document.getElementById("a0").innerHTML = arr[0];
+  document.getElementById("a1").innerHTML = arr[1];
+  document.getElementById("a2").innerHTML = arr[2];
+  document.getElementById("a3").innerHTML = arr[3];
+  document.getElementById("a4").innerHTML = arr[4];
+  document.getElementById("b0").innerHTML = arr[5];
+  document.getElementById("b1").innerHTML = arr[6];
+  document.getElementById("b2").innerHTML = arr[7];
+  document.getElementById("b3").innerHTML = arr[8];
+  document.getElementById("b4").innerHTML = arr[9];
+  document.getElementById("c0").innerHTML = arr[10];
+  document.getElementById("c1").innerHTML = arr[11];
+  document.getElementById("c2").innerHTML = arr[12];
+  document.getElementById("c3").innerHTML = arr[13];
+  document.getElementById("c4").innerHTML = arr[14];
+  document.getElementById("d0").innerHTML = arr[15];
+  document.getElementById("d1").innerHTML = arr[16];
+  document.getElementById("d2").innerHTML = arr[17];
+  document.getElementById("d3").innerHTML = arr[18];
+  document.getElementById("d4").innerHTML= arr[19];
+  document.getElementById("e0").innerHTML= arr[20];
+  document.getElementById("e1").innerHTML = arr[21];
+  document.getElementById("e2").innerHTML = arr[22];
+  document.getElementById("e3").innerHTML = arr[23];
+  document.getElementById("e4").innerHTML = arr[24];
+  }
+var brry =[];
+function a1()
 {
-    const arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25];
+brry.push(1);
+brry.sort();
+
+
+
+  document.getElementById("a1").style.backgroundColor = "teal";
+  document.getElementById("a1j").style.backgroundColor = "teal";
+}
+function a0()
+{
+brry.push(2);
+brry.sort();
+  document.getElementById("a0").style.backgroundColor = "teal";
+  document.getElementById("a0j").style.backgroundColor = "teal";
+}
+function a2()
+{
+brry.push(3);
+brry.sort();
+  document.getElementById("a2").style.backgroundColor = "teal";
+  document.getElementById("a2j").style.backgroundColor = "teal";
+}
+function a3()
+{
+brry.push(4);
+brry.sort();
+  document.getElementById("a3").style.backgroundColor = "teal";
+  document.getElementById("a3j").style.backgroundColor = "teal";
+}
+function a4()
+{
+brry.push(5);
+brry.sort();
+  document.getElementById("a4").style.backgroundColor = "teal";
+  document.getElementById("a4j").style.backgroundColor = "teal";
+}
+function b0()
+{
+brry.push(6);
+brry.sort();
+  document.getElementById("b0").style.backgroundColor = "teal";
+  document.getElementById("b0j").style.backgroundColor = "teal";
+}
+function b1()
+{
+brry.push(7);
+brry.sort();
+  document.getElementById("b1").style.backgroundColor = "teal";
+  document.getElementById("b1j").style.backgroundColor = "teal";
+}
+function b2()
+{
+brry.push(8);
+brry.sort();
+  document.getElementById("b2").style.backgroundColor = "teal";
+  document.getElementById("b2j").style.backgroundColor = "teal";
+}
+
+function b3()
+{
+brry.push(9);
+brry.sort();
+  document.getElementById("b3").style.backgroundColor = "teal";
+  document.getElementById("b3j").style.backgroundColor = "teal";
+}
+function b4()
+{
+brry.push(10);
+brry.sort();
+  document.getElementById("b4").style.backgroundColor = "teal";
+  document.getElementById("b4j").style.backgroundColor = "teal";
+}
+function c0()
+{
+brry.push(11);
+brry.sort();
+  document.getElementById("c0").style.backgroundColor = "teal";
+  document.getElementById("c0j").style.backgroundColor = "teal";
+}
+function c1()
+{
+brry.push(12);
+brry.sort();
+  document.getElementById("c1").style.backgroundColor = "teal";
+  document.getElementById("c1j").style.backgroundColor = "teal";
+}
+function c2()
+{
+brry.push(13);
+brry.sort();;
+  document.getElementById("c2").style.backgroundColor = "teal";
+  document.getElementById("c2j").style.backgroundColor = "teal";
+}
+function c3()
+{
+brry.push(14);
+brry.sort();
+  document.getElementById("c3").style.backgroundColor = "teal";
+  document.getElementById("c3j").style.backgroundColor = "teal";
+}
+function c4()
+{
+brry.push(15);
+brry.sort();
+  document.getElementById("c4").style.backgroundColor = "teal";
+  document.getElementById("c4j").style.backgroundColor = "teal";
+}
+function d0()
+{
+brry.push(16);
+brry.sort();
+  document.getElementById("d0").style.backgroundColor = "teal";
+  document.getElementById("d0j").style.backgroundColor = "teal";
+}
+function d1()
+{
+brry.push(17);
+brry.sort();
+  document.getElementById("d1").style.backgroundColor = "teal";
+  document.getElementById("d1j").style.backgroundColor = "teal";
+}
+function d2()
+{
+brry.push(18);
+brry.sort();
+  document.getElementById("d2").style.backgroundColor = "teal";
+  document.getElementById("d2j").style.backgroundColor = "teal";
+}
+function d3()
+{
+brry.push(19);
+brry.sort();
+  document.getElementById("d3").style.backgroundColor = "teal";
+  document.getElementById("d3j").style.backgroundColor = "teal";
+}
+function d4()
+{
+brry.push(20);
+brry.sort();
+  document.getElementById("d4").style.backgroundColor = "teal";
+  document.getElementById("d4j").style.backgroundColor = "teal";
+}
+function e0()
+{
+brry.push(21);
+brry.sort();
+  document.getElementById("e0").style.backgroundColor = "teal";
+  document.getElementById("e0j").style.backgroundColor = "teal";
+}
+function e1()
+{
+brry.push(22);
  
-    {
-      arr.sort (function(){return 0.5 -Math.random()});
-    }
-  document.getElementById("a0").value = arr[0];
-  document.getElementById("a1").value = arr[1];
-  document.getElementById("a2").value = arr[2];
-  document.getElementById("a3").value = arr[3];
-  document.getElementById("a4").value = arr[4];
-  document.getElementById("b0").value = arr[5];
-  document.getElementById("b1").value = arr[6];
-  document.getElementById("b2").value = arr[7];
-  document.getElementById("b3").value = arr[8];
-  document.getElementById("b4").value = arr[9];
-  document.getElementById("c0").value = arr[10];
-  document.getElementById("c1").value = arr[11];
-  document.getElementById("c2").value = arr[12];
-  document.getElementById("c3").value = arr[13];
-  document.getElementById("c4").value = arr[14];
-  document.getElementById("d0").value = arr[15];
-  document.getElementById("d1").value = arr[16];
-  document.getElementById("d2").value = arr[17];
-  document.getElementById("d3").value = arr[18];
-  document.getElementById("d4").value = arr[19];
-  document.getElementById("e0").value = arr[20];
-  document.getElementById("e1").value = arr[21];
-  document.getElementById("e2").value = arr[22];
-  document.getElementById("e3").value = arr[23];
-  document.getElementById("e4").value = arr[24];
-  
-  
+brry.sort();
+document.getElementById("e1").style.backgroundColor = "teal";
+  document.getElementById("e1j").style.backgroundColor = "teal";
+}
+function e2()
+{
+  brry.push(23);
+brry.sort();
+  document.getElementById("e2").style.backgroundColor = "teal";
+  document.getElementById("e2j").style.backgroundColor = "teal";
+}
+function e3()
+{
+  brry.push(24);
+  brry.sort();
+  document.getElementById("e3").style.backgroundColor = "teal";
+  document.getElementById("e3j").style.backgroundColor = "teal";
+}
+function e4()
+{
+  brry.push(25);
+  brry.sort();
+  document.getElementById("e4").style.backgroundColor = "teal";
+  document.getElementById("e4j").style.backgroundColor = "teal";
+}
+function bingocheck ()
+{
   
 }
