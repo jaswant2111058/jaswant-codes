ecd986053b3cdfc83e2009056dd81d54652348a7
jaswant2111058 <jaswant2111058@akgec.ac.in>
2023-01-10 00:05:26 +0530

    bingo

--- BINGO/jassi.js
+++ BINGO/jassi.js
@@ -18,8 +18,8 @@ var arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25];
   document.getElementById("d1").innerHTML = "I";
   document.getElementById("d2").innerHTML = "N";
   document.getElementById("d3").innerHTML = "G";
-  document.getElementById("d4").innerHTML= "O";
-  document.getElementById("e0").innerHTML= "B";
+  document.getElementById("d4").innerHTML = "O";
+  document.getElementById("e0").innerHTML = "B";
   document.getElementById("e1").innerHTML = "I";
   document.getElementById("e2").innerHTML = "N";
   document.getElementById("e3").innerHTML = "G";
@@ -27,6 +27,7 @@ var arr = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25];
 function randomgenrator()
   {
    arr.sort(function(){return 0.5- Math.random()});
+
   document.getElementById("a0").innerHTML = arr[0];
   document.getElementById("a1").innerHTML = arr[1];
   document.getElementById("a2").innerHTML = arr[2];
@@ -46,194 +47,414 @@ function randomgenrator()
   document.getElementById("d1").innerHTML = arr[16];
   document.getElementById("d2").innerHTML = arr[17];
   document.getElementById("d3").innerHTML = arr[18];
-  document.getElementById("d4").innerHTML= arr[19];
-  document.getElementById("e0").innerHTML= arr[20];
+  document.getElementById("d4").innerHTML = arr[19];
+  document.getElementById("e0").innerHTML = arr[20];
   document.getElementById("e1").innerHTML = arr[21];
   document.getElementById("e2").innerHTML = arr[22];
   document.getElementById("e3").innerHTML = arr[23];
   document.getElementById("e4").innerHTML = arr[24];
+   } 
+
+
+   var map = new Array();
+function call()
+{
+   for( let i=0;i<25;i++)
+   {
+      map[arr[i]]=i+1;
+   }
   }
+
+
 var brry =[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
+var sum =[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0];
 
 function a0()
 {
 brry[0]=1;
+sum[0]=1;
+
   document.getElementById("a0").style.backgroundColor = "teal";
   document.getElementById("a0j").style.backgroundColor = "teal";
+  const a0 = document.getElementById("a0").innerHTML;
+  
+  server_fetch(a0);
+  
+  document.getElementById("a0").disabled=true;
+
   bcheck()
 }
-function a1()
+function a1(num)
 {
 brry[1]=2
+sum[1]=1;
   document.getElementById("a1").style.backgroundColor = "teal";
   document.getElementById("a1j").style.backgroundColor = "teal";
   bcheck();
+  const a0 = document.getElementById("a1").innerHTML;
+  if(num!=0)
+  {
+  server_fetch(a0);
+  }
+  document.getElementById("a1").disabled=true;
 }
-function a2()
+function a2(num)
 {
 brry[2]=3;
+sum[2]=1; 
   document.getElementById("a2").style.backgroundColor = "teal";
   document.getElementById("a2j").style.backgroundColor = "teal";
   bcheck();
+  const a0 = document.getElementById("a2").innerHTML;
+  
+  if(num!=0)
+  {
+  server_fetch(a0);
+  }
+  
+  document.getElementById("a2").disabled=true;
 }
-function a3()
+function a3(num)
 {
 brry[3]=4;
+sum[3]=1;
   document.getElementById("a3").style.backgroundColor = "teal";
   document.getElementById("a3j").style.backgroundColor = "teal";
   bcheck();
+  const a0 = document.getElementById("a3").innerHTML;
+  if(num!=0)
+  {
+  server_fetch(a0);
+  }
+  
+  document.getElementById("a3").disabled=true;
 }
-function a4()
+function a4(num)
 {
 brry[4]=5;
+sum[4]=1;
   document.getElementById("a4").style.backgroundColor = "teal";
   document.getElementById("a4j").style.backgroundColor = "teal";
   bcheck();
+  const a0 = document.getElementById("a4").innerHTML;
+  if(num!=0)
+  {
+  server_fetch(a0);
+  }
+    
+  document.getElementById("a4").disabled=true;
 }
-function b0()
+function b0(num)
 {
 brry[5]=6;
+sum[5]=1;
   document.getElementById("b0").style.backgroundColor = "teal";
   document.getElementById("b0j").style.backgroundColor = "teal";
   bcheck();
+  const a0 = document.getElementById("b0").innerHTML;
+  if(num!=0)
+  {
+  server_fetch(a0);
+  }
+  document.getElementById("b0").disabled=true;
 }
-function b1()
+function b1(num)
 {
 brry[6]=7;
+sum[6]=1;
   document.getElementById("b1").style.backgroundColor = "teal";
   document.getElementById("b1j").style.backgroundColor = "teal";
   bcheck();
+  const a0 = document.getElementById("b1").innerHTML;
+  if(num!=0)
+  {
+  server_fetch(a0);
+  }
+    
+  document.getElementById("b1").disabled=true;
 }
-function b2()
+function b2(num)
 {
 brry[7]=8;
+sum[7]=1;
   document.getElementById("b2").style.backgroundColor = "teal";
   document.getElementById("b2j").style.backgroundColor = "teal";
   bcheck();
+  const a0 = document.getElementById("b2").innerHTML;
+  if(num!=0)
+  {
+  server_fetch(a0);
+  }
+    
+  document.getElementById("b2").disabled=true;
 }
 
-function b3()
+function b3(num)
 {
 brry[8]=9;
+sum[8]=1;
   document.getElementById("b3").style.backgroundColor = "teal";
   document.getElementById("b3j").style.backgroundColor = "teal";
   bcheck();
+  const a0 = document.getElementById("b3").innerHTML;
+  if(num!=0)
+  {
+  server_fetch(a0);
+  }
+    
+  document.getElementById("b3").disabled=true;
 }
-function b4()
+function b4(num)
 {
 brry[9]=10;
+sum[9]=1;
   document.getElementById("b4").style.backgroundColor = "teal";
   document.getElementById("b4j").style.backgroundColor = "teal";
   bcheck();
+  const a0 = document.getElementById("b4").innerHTML;
+  if(num!=0)
+  {
+  server_fetch(a0);
+  }
+  
+  
+  document.getElementById("b4").disabled=true;
 }
-function c0()
+function c0(num)
 {
 brry[10]=11;
+sum[10]=1;
   document.getElementById("c0").style.backgroundColor = "teal";
   document.getElementById("c0j").style.backgroundColor = "teal";
   bcheck();
+  const a0 = document.getElementById("c0").innerHTML;
+  if(num!=0)
+  {
+  server_fetch(a0);
+  }
+  
+  
+  document.getElementById("c0").disabled=true;
 }
-function c1()
+function c1(num)
 {
 brry[11]=12;
-
+sum[11]=1;
   document.getElementById("c1").style.backgroundColor = "teal";
   document.getElementById("c1j").style.backgroundColor = "teal";
   bcheck();
+  const a0 = document.getElementById("c1").innerHTML;
+  if(num!=0)
+  {
+  server_fetch(a0);
+  }
+  
+  
+  document.getElementById("c1").disabled=true;
 }
-function c2()
+function c2(num)
 {
 brry[12]=13;
-
+sum[12]=1;
   document.getElementById("c2").style.backgroundColor = "teal";
   document.getElementById("c2j").style.backgroundColor = "teal";
   bcheck();
+  const a0 = document.getElementById("c2").innerHTML;
+  if(num!=0)
+  {
+  server_fetch(a0);
+  }
+  
+  
+  document.getElementById("c2").disabled=true;
 }
-function c3()
+function c3(num)
 {
 brry[13]=14;
+sum[13]=1;
   document.getElementById("c3").style.backgroundColor = "teal";
   document.getElementById("c3j").style.backgroundColor = "teal";
   bcheck();
+  const a0 = document.getElementById("c3").innerHTML;
+  if(num!=0)
+  {
+  server_fetch(a0);
+  }
+  
+  
+  document.getElementById("c3").disabled=true;
 }
-function c4()
+function c4(num)
 {
 brry[14]=15;
+sum[14]=1;
   document.getElementById("c4").style.backgroundColor = "teal";
   document.getElementById("c4j").style.backgroundColor = "teal";
   bcheck();
+  const a0 = document.getElementById("c4").innerHTML;
+  if(num!=0)
+  {
+  server_fetch(a0);
+  }
+  
+  
+  document.getElementById("c4").disabled=true;
 }
-function d0()
+function d0(num)
 {
 brry[15]=16;
+sum[15]=1;
   document.getElementById("d0").style.backgroundColor = "teal";
   document.getElementById("d0j").style.backgroundColor = "teal";
   bcheck();
+  const a0 = document.getElementById("d0").innerHTML;
+  if(num!=0)
+  {
+  server_fetch(a0);
+  }
+  
+  
+  document.getElementById("d0").disabled=true;
 }
-function d1()
+function d1(num)
 {
 brry[16]=17;
+sum[16]=1;
   document.getElementById("d1").style.backgroundColor = "teal";
   document.getElementById("d1j").style.backgroundColor = "teal";
   bcheck();
+  const a0 = document.getElementById("d1").innerHTML;
+  if(num!=0)
+  {
+  server_fetch(a0);
+  }
+  
+  
+  document.getElementById("d1").disabled=true;
 }
-function d2()
+function d2(num)
 {
 brry[17]=18;
+sum[17]=1;
   document.getElementById("d2").style.backgroundColor = "teal";
   document.getElementById("d2j").style.backgroundColor = "teal";
   bcheck();
+  const a0 = document.getElementById("d2").innerHTML;
+  if(num!=0)
+  {
+  server_fetch(a0);
+  }
+  
+  
+  document.getElementById("d2").disabled=true;
 }
-function d3()
+function d3(num)
 {
 brry[18]=19;
+sum[18]=1;
   document.getElementById("d3").style.backgroundColor = "teal";
   document.getElementById("d3j").style.backgroundColor = "teal";
   bcheck();
+  const a0 = document.getElementById("d3").innerHTML;
+  if(num!=0)
+  {
+  server_fetch(a0);
+  }
+  
+  document.getElementById("d3").disabled=true;
 }
-function d4()
+function d4(num)
 {
 brry[19]=20;
+sum[19]=1;
   document.getElementById("d4").style.backgroundColor = "teal";
   document.getElementById("d4j").style.backgroundColor = "teal";
   bcheck();
+  const a0 = document.getElementById("d4").innerHTML;
+  if(num!=0)
+  {
+  server_fetch(a0);
+  }
+  
+  document.getElementById("d4").disabled=true;
 }
-function e0()
+function e0(num)
 {
 brry[20]=21;
+sum[20]=1;
   document.getElementById("e0").style.backgroundColor = "teal";
   document.getElementById("e0j").style.backgroundColor = "teal";
   bcheck();
+  const a0 = document.getElementById("e0").innerHTML;
+  if(num!=0)
+  {
+  server_fetch(a0);
+  }
+  
+  document.getElementById("e0").disabled=true;
 }
-function e1()
+function e1(num)
 {
 brry[21]=22;
- 
+sum[21]=1;
 document.getElementById("e1").style.backgroundColor = "teal";
   document.getElementById("e1j").style.backgroundColor = "teal";
   bcheck();
+  const a0 = document.getElementById("e1").innerHTML;
+  if(num!=0)
+  {
+  server_fetch(a0);
+  }
+  
+  
+  document.getElementById("e1").disabled=true;
 }
-function e2()
+function e2(num)
 {
   brry[22]=23;
- 
+  sum[22]=1;
   document.getElementById("e2").style.backgroundColor = "teal";
   document.getElementById("e2j").style.backgroundColor = "teal";
   bcheck();
+  const a0 = document.getElementById("e2").innerHTML;
+  if(num!=0)
+  {
+  server_fetch(a0);
+  }
+  
+  
+  document.getElementById("e2").disabled=true;
 }
-function e3()
+function e3(num)
 {
   brry[23]=24;
+  sum[23]=1;
     document.getElementById("e3").style.backgroundColor = "teal";
   document.getElementById("e3j").style.backgroundColor = "teal";
   bcheck();
+  const a0 = document.getElementById("e3").innerHTML;
+  if(num!=0)
+  {
+  server_fetch(a0);
+  }
+  
+  
+  document.getElementById("e3").disabled=true;
 }
-function e4()
+function e4(num)
 {
   brry[24]=25;
+  sum[24]=1;
     document.getElementById("e4").style.backgroundColor = "teal";
   document.getElementById("e4j").style.backgroundColor = "teal";
   bcheck();
+  const a0 = document.getElementById("e4").innerHTML;
+  if(num!=0)
+  {
+  server_fetch(a0);
+  }
+  
+  document.getElementById("e4").disabled=true; 
 }
 function bingocheck ()
 {
@@ -255,13 +476,7 @@ function search()
   }
   return x;
 }
-function see()
-{
-    for(let i=0;i<brry.length;i++)
-      {
-        console.log(brry[i]);
-      }
-}
+
 
 function rowcheck()
 { 
@@ -501,5 +716,113 @@ function suff()
   window.alert("YOU HAVE NOT COMLETED THE GAME")
 
  }
+
+ function callback(num)
+ {    
+      const callnum=map[num];      
+    const n=0;
+      switch(callnum)
+      {
+        case 1: a0(n);
+        break;
+        case 2: a1(n);
+        break;
+        case 3: a2(n);
+        break;
+        case 4: a3(n);
+        break;
+        case 5: a4(n);
+        break;
+        case 6: b0(n);
+        break;
+        case 7: b1(n);
+        break;
+        case 8: b2(n);
+        break;
+        case 9: b3(n);
+        break;
+        case 10: b4(n);
+        break;
+        case 11: c0(n);
+        break;
+        case 12: c1(n);
+        break;
+        case 13: c2(n);
+        break;
+        case 14: c3(n);
+        break;
+        case 15: c4(n);
+        break;
+        case 16: d0(n);
+        break;
+        case 17: d1(n);
+        break;
+        case 18: d2(n);
+        break;
+        case 19: d3(n);
+        break;
+        case 20: d4(n);
+        break;
+        case 21: e0(n);
+        break;
+        case 22: e1(n);
+        break;
+        case 23: e2(n);
+        break;
+        case 24: e3(n);
+        break;
+        case 25: e4(n);
+        break;
+        
+      }
+ }
+ function server_fetch(num)
+ {
+  socket.emit("num",num);
+ }
+socket.on("data",data=>{
+  
+    call();
+    callback(data.num)
+  
+    
+  
+})
+// function even()
+// { var sm= 0
+//   for(let i=0;i<24;i++)
+//   {
+//     sm=sm+sum[i];
+//   }
+//   if(sm%2==0)
+//   {
+//   document.getElementById("a0").disabled = true
+//   document.getElementById("a1").disabled = true
+//   document.getElementById("a2").disabled = true
+//   document.getElementById("a3").disabled = true
+//   document.getElementById("a4").disabled = true
+//   document.getElementById("b0").disabled = true
+//   document.getElementById("b1").disabled = true
+//   document.getElementById("b2").disabled = true
+//   document.getElementById("b3").disabled = true
+//   document.getElementById("b4").disabled = true
+//   document.getElementById("c0").disabled = true;
+//   document.getElementById("c1").disabled = true;
+//   document.getElementById("c2").disabled = true;
+//   document.getElementById("c3").disabled = true;
+//   document.getElementById("c4").disabled = true;
+//   document.getElementById("d0").disabled = true;
+//   document.getElementById("d1").disabled = true;
+//   document.getElementById("d2").disabled = true;
+//   document.getElementById("d3").disabled = true;
+//   document.getElementById("d4").disabled = true;
+//   document.getElementById("e0").disabled = true;
+//   document.getElementById("e1").disabled = true;
+//   document.getElementById("e2").disabled = true;
+//   document.getElementById("e3").disabled = true;
+//   document.getElementById("e4").disabled = true;
+//   }
+// }
+
 
     
